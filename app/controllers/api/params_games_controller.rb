d6dbf4fc73b06afdd883c03452a572ed4eb7983a
ThundaHorse <abeeeeekim@gmail.com>
2019-05-03 10:25:29 -0500

    added guessing game and name

--- app/controllers/api/params_games_controller.rb
+++ app/controllers/api/params_games_controller.rb
@@ -1,27 +1,29 @@
 class Api::ParamsGamesController < ApplicationController
-  def guess_number 
-    @guess = params[:guess]
-    @results = []
 
-    if @guess.to_i > 35 
-      @results << "Too high"
-    elsif @guess.to_i < 35 
-      @results << "Too low"
-    elsif @guess.to_i == 35 
-      @results << "Correct!" 
+  def guess_number 
+    @guess = params[:guess].to_i
+    @results = ''
+
+    if @guess > 35 
+      @results = "Too high"
+    elsif @guess < 35 
+      @results = "Too low"
+    else 
+      @results = "Correct!" 
     end 
 
     render 'number.json.jbuilder'
   end 
 
+
   def display_name 
     @name = params[:name].upcase
-    @message = [] 
-
+    
     if @name[0] == 'A' 
-      @message << "Hey, your name starts with the first letter of the alphabet!"
+      @message = "Hey, your name starts with the first letter of the alphabet!"
+    else 
+      @message = "Move along now, git git"
     end 
-
 
     render 'name_view.json.jbuilder'
   end 
