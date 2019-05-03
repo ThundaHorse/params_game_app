4970bba80a804e686bbafca14f710ce72246125b
ThundaHorse <abeeeeekim@gmail.com>
2019-05-03 10:59:56 -0500

    removed extra /

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get '/guessing_game' => 'params_games#guess_number'
     get '/show_name' => 'params_games#display_name'
-    get '/guessing_game/:number/' => 'params_games#segment_number'
+    get '/guessing_game/:number' => 'params_games#segment_number'
   end
 end
